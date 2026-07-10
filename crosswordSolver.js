04a86d6c3905e6e9e854264e14d8297bad6e40f2
gelafros <elafrosg@hotmail.gr>
2026-07-10 16:17:18 +0300

    feat:add input validating logic

---
+++ crosswordSolver.js
@@ -0,0 +1,51 @@
+function parse(emptyPuzzle,words){
+    
+
+
+
+console.log()
+}
+
+function validateInput(emptyPuzzle,words){
+    if ((typeof emptyPuzzle !== 'string') || (!Array.isArray(words))){
+        console.log('Error')
+        return
+    }
+
+    if (emptyPuzzle.length === 0 || words.length === 0){
+        console.log('Error')
+        return
+    }
+
+    const regex = /[^\.\d\n]/
+    if (regex.test(emptyPuzzle)){
+        console.log('Error')
+        return
+    }
+
+    const seen = new Set(words)
+    if (seen.size !== words.length){
+        console.log('Error')
+        return
+    }
+
+    const rows = emptyPuzzle.split('\n')
+    const isRectangular = rows.every(row => row.length === rows[0].length);
+    if (!isRectangular) {
+        console.log('Error');
+        return;
+        }
+
+    const singleDigits = emptyPuzzle.match((/\d/g));
+    if (!singleDigits) {
+        console.log('Error');
+        return;
+    }
+    const numOfWords = singleDigits.reduce((total, curr) => total + Number(curr), 0);
+    if (numOfWords !== words.length) {
+        console.log('Error');
+        return;
+    } 
+
+
+}    
